4b3933da92d2196303530b679e1515bf8fb3d6b2
Mayer MICHEL <mayer.mohib@gmail.com>
2025-04-09 10:55:08 +0200

    online router + appRoute

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import OfflineRouter from './router/OfflineRouter.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { AuthContextProvider } from './contexts/AuthContext.jsx'
+import ButtonLoader from './components/Loader/ButtonLoader.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContextProvider>
       <RouterProvider router={OfflineRouter}/>
+      
     </AuthContextProvider>
   </StrictMode>,
 )
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { PROFILE_INFOS} from "../constants/appConstant";
+import PageLoader from "../components/Loader/PageLoader";
+import { RouterProvider } from "react-router-dom";
+import OnlineRouter from "./OnlineRouter";
+import OfflineRouter from "./OfflineRouter";
+import { useAuthContext } from "../contexts/AuthContext";
+
+// Création d'un mini context pour la session
+const SessionContext = createContext({inSession: false});
+
+// Hook pour l'utiliser le contexte de session 
+export const useSessionContext = () => useContext(SessionContext);
+
+const AppRouter = () => {
+    // On crée un state pour gere la session 
+    const [inSession, setInSession] = useState(null);
+    // On récupère grace au hook les données de notre contexte d'authentification
+    const { profileId, setProfileId, setName } = useAuthContext();
+    // On récupère les données de l'utilisateur dans le localStorage
+    const profileInfo = JSON.parse(localStorage.getItem(PROFILE_INFOS));
+
+    useEffect(() => {
+      const checkUserSession = async () => {
+        if(profileInfo){
+            setProfileId(profileInfo.profileId);
+            setName(profileInfo.name);
+            setInSession(true);
+        }else{
+            setInSession(false);
+        }
+      }
+
+      checkUserSession();
+
+    }, [profileId])
+    
+    // On affiche le loader le temps du chargement
+    if(inSession === null){
+        return <PageLoader />
+    }
+
+    return (
+        <SessionContext.Provider value = {{inSession}}>
+            <RouterProvider router={inSession ? OnlineRouter : OfflineRouter} />
+        </SessionContext.Provider>
+    )
+
+}
+
+export default AppRouter;
