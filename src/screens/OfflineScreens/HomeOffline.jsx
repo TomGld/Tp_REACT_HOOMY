377e505349eb36f0f7ad0430fcf8c7fe34470874
TomGld <fouet.tom.pro@gmail.com>
2025-04-09 14:45:51 +0200

    add login; update onglet name + logo; add taildwind; add component login; update apiConstant; add PageLoader; update readme;

--- src/screens/OfflineScreens/HomeOffline.jsx
+++ src/screens/OfflineScreens/HomeOffline.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IMAGES_URL } from '../../constants/apiConstant'
+import { API_ROOT, IMAGES_URL, LOGOS_URL } from '../../constants/apiConstant'
 import { Outlet } from 'react-router-dom'
 
 
@@ -9,7 +9,7 @@ const HomeOffline = () => {
     <div className='flex flex-col w-screen h-screen items-center px-4 bg-black bgopacity-50 bg-center bg-contain'>
       <div className='w-full md:w-3/4n lg:w-1/2 h-screen flex flex-col justify-center items-center bg-black_05'>
         <div className='w-full flex justify-center items-center py-8 rounded-lg'>
-          <img src={`${IMAGES_URL}/logo/LogoX2.png`} alt='logo' className='h-20 object-contain' />
+          <img src={`${LOGOS_URL}/LogoX2.png`} alt='logo' className='h-20 object-contain' />
         </div>
         <div className='flex justify-center items-center px-4 w-full rounded-lg mt-4'>
           <Outlet />
