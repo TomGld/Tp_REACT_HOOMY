1d7ac3126263ac62787a14fb498c589d9fb0f744
Mayer MICHEL <mayer.mohib@gmail.com>
2025-04-25 02:56:30 +0200

    modif vibeSlice

--- src/store/vibe/vibeSelector.js
+++ src/store/vibe/vibeSelector.js
@@ -2,12 +2,12 @@ import { createSelector } from "@reduxjs/toolkit";
 
 const selectLoading = (state) => state.vibe.loading;
 const selectVibe = (state) => state.vibe.vibe;
-const selectVibeDetail = (state) => state.vibe.vibeDetail;
+
 
 const selectVibeData = createSelector(
 
-    [selectLoading, selectVibe, selectVibeDetail],
-    (loading, vibe, vibeDetail) => ({ loading, vibe, vibeDetail })
+    [selectLoading, selectVibe],
+    (loading, vibe) => ({ loading, vibe })
 
 );
 export default selectVibeData;
--- src/store/vibe/vibeSlice.js
+++ src/store/vibe/vibeSlice.js
@@ -2,37 +2,49 @@ import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { API_URL } from "../../constants/apiConstant";
 
-const VibeSlice = createSlice({
+const vibeSlice = createSlice({
     name: "vibe",
     initialState: {
         loading: false,
         vibe: [],
-        vibeDetail: {}
     },
     reducers: {
         setLoading: (state, action) => {
             state.loading = action.payload;
         },
-        setVibe: (state, action) => {
+        setVibes: (state, action) => {
             state.vibe = action.payload;
-        },
-        setVibeDetail: (state, action) => {
-            state.vibeDetail = action.payload;
-        },
+        }
     },
 });
-export const { setLoading, setVibe, setVibeDetail } = VibeSlice.actions;
+export const { setLoading, setVibes } = vibeSlice.actions;
 
+// Méthode pour récupérer les vibes
 export const fetchVibe = () => async (dispatch) => {
     try {
         dispatch(setLoading(true));
         const response = await axios.get(`${API_URL}/vibes?page=1`);
-        dispatch(setVibe(response.data.member));
+        dispatch(setVibes(response.data.member));
     } catch (error) {
-        console.error(`Error fetching vibe: ${error}`);
+        console.error(`Error fetching vibes: ${error}`);
     } finally {
         dispatch(setLoading(false));
     }
 }
 
-export default VibeSlice.reducer;
+// Méthode pour récupérer vibe par id
+export const fetchVibeById = (id) => async (dispatch) => {
+    try {
+        dispatch(setLoading(true));
+        const response = await axios.get(`${API_URL}/vibes/${id}`);
+        dispatch(setVibes([response.data]));
+    } catch (error) {
+        console.error(`Error fetching vibe by ID: ${error}`);
+    } finally {
+        dispatch(setLoading(false));
+    }
+}
+
+
+
+export default vibeSlice.reducer;
